12d8e311717d2e9882a5dcc0a955318259e3494b
Tran Khanh <tran_khanh@outlook.com>
2022-05-08 15:08:25 +0700

    add nextjs feature

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,24 @@
 import '../styles/globals.css'
+import 'prismjs/themes/prism.css'
+import 'react-notion-x/src/styles.css'
+import 'katex/dist/katex.min.css'
+import '@/styles/globals.css'
+import '@/styles/notion.css'
+import { LocaleProvider } from '@/lib/locale'
+import Scripts from '@/components/Scripts'
+
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (<>
+    <Scripts />
+    <LocaleProvider>
+      <>
+        <Component {...pageProps} />
+      </>
+    </LocaleProvider>
+  </>
+  )
+
 }
 
 export default MyApp
