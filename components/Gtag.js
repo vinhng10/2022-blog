d843743d367f7ad3d337e28659257291fa4ad4be
Tran Khanh <tran_khanh@outlook.com>
2022-05-08 22:02:09 +0700

    update Gtag

---
+++ components/Gtag.js
@@ -0,0 +1,19 @@
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
+import * as gtag from '@/lib/gtag'
+
+const Gtag = () => {
+    const router = useRouter()
+    useEffect(() => {
+        const handleRouteChange = url => {
+            gtag.pageview(url)
+        }
+        router.events.on('routerChangeComplete', handleRouteChange)
+        return () => {
+            router.events.off('routeChangeComplete', handleRouteChange)
+        }
+    }, [router.events])
+    return null
+}
+
+export default Gtag
