b1139a4b4b92d8830710d22c88cd7ce8ec0c6064
Tran Khanh <tran_khanh@outlook.com>
2022-05-13 18:44:49 +0700

    Remove Rss feature

--- pages/feed.js
+++ pages/feed.js
@@ -1,11 +1,11 @@
 import { getAllPosts } from '@/lib/notion'
-import { generateRss } from '@/lib/rss'
+// import { generateRss } from '@/lib/rss'
 export async function getServerSideProps ({ res }) {
   res.setHeader('Content-Type', 'text/xml')
   const posts = await getAllPosts({ includePages: false })
   const latestPosts = posts.slice(0, 10)
-  const xmlFeed = await generateRss(latestPosts)
-  res.write(xmlFeed)
+  // const xmlFeed = await generateRss(latestPosts)
+  // res.write(xmlFeed)
   res.end()
   return {
     props: {}
